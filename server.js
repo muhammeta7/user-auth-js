2b8b77f3ab875703104d4841794eddb09e018599
muhammeta7 <muhammeta7@gmail.com>
2021-02-02 21:37:02 -0500

    Fixed bug with displaying html page.

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // app.js
+var http = require('http');
 const express = require('express')
-
+var fs = require('fs');
+const port = 3000;
 // Create Express app
 const app = express()
 
-// A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
+})
 
-// Start the Express server
-app.listen(3000, () => console.log('Server running on port 3000!'))
+server.listen(port, () => console.log('Server running on port 3000!'))
+
+// // Start the Express server
+// app.listen(3000, () => console.log('Server running on port 3000!'))
+
+
+
+
+
+
